802979248ddbf62aacc3442cf4c720bc12c08a0e
woodenman89 <74547316+woodenman89@users.noreply.github.com>
2020-12-10 14:21:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
+function pictureChange() {
+  var randomNumber1 = Math.floor(Math.random() * 2) + 1;
+  document.querySelector(".img").setAttribute("src", "images/cat" + randomNumber1 + ".png")
 
-var randomNumber1 = Math.floor(Math.random() * 2) +1;
-document.querySelector(".img").setAttribute("src","images/cat" + randomNumber1 + ".png")
-
-if (randomNumber1 === 1 ) {
-document.querySelector("h1").innerHTML = "GO Cat Go!!!";
-}
-else {
-document.querySelector("h1").innerHTML = "Not Today";
+  if (randomNumber1 === 1) {
+    document.querySelector("h1").innerHTML = "GO Cat Go!";
+  } else {
+    document.querySelector("h1").innerHTML = "Not Today";
+  }
 }
