1f44b33e0e5e45bd126ddf49439312e58b1f410a
woodenman89 <74547316+woodenman89@users.noreply.github.com>
2020-12-14 11:44:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 function pictureChange() {
   var randomNumber1 = Math.floor(Math.random() * 2) + 1;
   document.querySelector(".img").setAttribute("src", "images/cat" + randomNumber1 + ".png")
+    var audio = new Audio('meow.mp3');
+    audio.play();
 
   if (randomNumber1 === 1) {
     document.querySelector("h1").innerHTML = "GO Cat Go!";
